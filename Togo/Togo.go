0a4b9a701f82653d7f2daca8e64a6c261047bb61
pya-h <p4ya.py@gmail.com>
2024-01-29 02:22:17 +0330

    starting out to modify the serverless version of the bot into longpoll version.

---
+++ Togo/Togo.go
@@ -0,0 +1,364 @@
+package Togo
+
+import (
+	// chrono "github.com/gochrono/chrono"
+	"database/sql"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	_ "github.com/lib/pq" // postgres
+)
+
+var lastUsedId uint64 = 0
+
+// var taskScheduler chrono.TaskScheduler = chrono.NewDefaultTaskScheduler()
+
+// ---------------------- Date/Time Struct & Date Receivers --------------------------------
+type Date struct{ time.Time }
+
+func (d *Date) Get() string {
+
+	return fmt.Sprintf("%d-%d-%d\t%d:%d", d.Year(), d.Month(), d.Day(), d.Hour(), d.Minute())
+}
+func (d *Date) Short() string {
+
+	return fmt.Sprintf("%d-%d-%d", d.Year(), d.Month(), d.Day())
+}
+
+func Now() time.Time {
+	if timeZone, err := time.LoadLocation("Asia/Tehran"); err == nil {
+		return time.Now().In(timeZone)
+	}
+	return time.Now()
+}
+
+func Today() Date {
+	return Date{Now()}
+}
+
+// ---------------------- Togo Struct & Togo Receivers--------------------------------
+type Togo struct {
+	Id          uint64
+	Title       string
+	Description string
+	Weight      uint16
+	Progress    uint8
+	Extra       bool
+	Date        Date
+	Duration    time.Duration
+	OwnerId     int64 // telegram id
+}
+
+func (togo *Togo) Save() uint64 {
+	/*const CREATE_TABLE_QUERY string = `CREATE TABLE IF NOT EXISTS togos (id SERIAL PRIMARY KEY, owner_id BIGINT NOT NULL,
+	title VARCHAR(64) NOT NULL, description VARCHAR(256), weight INTEGER, extra INTEGER,
+	progress INTEGER, date timestamp with time zone, duration INTEGER)`*/
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	/*if _, err := db.Exec(CREATE_TABLE_QUERY); err != nil {
+		panic(err)
+	}*/
+	extra := 0
+	if togo.Extra {
+		extra = 1
+	}
+	if res, err := db.Exec("INSERT INTO togos (owner_id, title, description, weight, extra, progress, date, duration) VALUES ($1, $2::varchar, $3::varchar, $4, $5, $6, $7, $8)",
+		togo.OwnerId, togo.Title, togo.Description, togo.Weight, extra, togo.Progress,
+		togo.Date.Time, togo.Duration.Minutes()); err != nil {
+		panic(err)
+	} else if id, e := res.LastInsertId(); e == nil {
+		return uint64(id)
+	}
+	return 0
+}
+
+func (togo *Togo) Schedule() {
+	/*_, err := taskScheduler.Schedule(func(ctx context.Context) {
+		fmt.Println("\nYour Next Togo:\n", togo.ToString(), "\n> ")
+		fmt.Print()
+	}, chrono.WithTime(togo.Date.Time))
+
+	if err != nil {
+		panic(err)
+	} else {
+		// log.Println("Togo: ", togo.Title, " Successfully scheduled for: ", togo.Date.Get())
+	}*/
+
+}
+
+func isCommand(term string) bool {
+	return term == "+" || term == "%" || term == "#" || term == "$"
+}
+
+func (togo *Togo) setFields(terms []string) {
+	numOfTerms := len(terms)
+	for i := 1; i < numOfTerms && !isCommand(terms[i]); i++ {
+		switch terms[i] {
+		case "=", "+w":
+			i++
+
+			if _, err := fmt.Sscan(terms[i], &togo.Weight); err != nil {
+				panic(err)
+			}
+
+		case ":", "+d":
+			i++
+			togo.Description = terms[i]
+		case "+x":
+			togo.Extra = true
+		case "-x":
+			togo.Extra = false
+		case "+p":
+			i++
+
+			if _, err := fmt.Sscan(terms[i], &togo.Progress); err != nil {
+				panic(err)
+			} else if togo.Progress > 100 {
+				togo.Progress = 100
+			}
+		case "@":
+			// im++
+			i++
+			today := Now()
+			var delta int
+			if _, err := fmt.Sscan(terms[i], &delta); err != nil {
+				panic(err)
+			}
+			today = today.AddDate(0, 0, delta)
+			i++
+			temp := strings.Split(terms[i], ":")
+			var hour, min int
+			if _, err := fmt.Sscan(temp[0], &hour); err != nil {
+				panic(err)
+			} else if hour >= 24 || hour < 0 {
+				panic("Hour part must be between 0 and 23!")
+			}
+			if _, err := fmt.Sscan(temp[1], &min); err != nil {
+				panic(err)
+			} else if min >= 60 || min < 0 {
+				panic("Minute part must be between 0 and 59!")
+			}
+			if locale, err := time.LoadLocation("Asia/Tehran"); err == nil {
+
+				togo.Date = Date{time.Date(today.Year(), today.Month(), today.Day(), hour, min, 0, 0, locale)}
+			} else {
+				togo.Date = Date{time.Date(today.Year(), today.Month(), today.Day(), hour, min, 0, 0, time.Local)}
+
+			}
+			// get the actual date here
+		case "->":
+			i++
+			if _, err := fmt.Sscan(terms[i], &togo.Duration); err != nil {
+				panic(err)
+			} else if togo.Duration > 0 {
+				togo.Duration *= time.Minute
+			} else {
+				panic("Duration must be positive integer!")
+			}
+		}
+
+	}
+
+}
+
+func (togo *Togo) Update(ownerID int64) {
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	extra := 0
+	if togo.Extra {
+		extra = 1
+	}
+	if _, err := db.Exec("UPDATE togos SET description=$1, weight=$2, extra=$3, progress=$4, date=$5, duration=$6 WHERE id=$7 AND owner_id=$8", // TODO: check ownerId? (no need)
+		togo.Description, togo.Weight, extra, togo.Progress, togo.Date.Time, togo.Duration.Minutes(), togo.Id, ownerID); err != nil {
+		panic(err)
+	}
+}
+
+func (togo *Togo) ToString() string {
+	return fmt.Sprintf("Togo #%d) %s:\t%s\nWeight: %d\nExtra: %t\nProgress: %d\nAt: %s, about %.1f minutes",
+		togo.Id, togo.Title, togo.Description, togo.Weight, togo.Extra, togo.Progress, togo.Date.Get(), togo.Duration.Minutes())
+}
+
+// ---------------------- TogoList Type & Togo Receivers--------------------------------
+type TogoList []Togo
+
+func (togos TogoList) ToString() (result []string) {
+	//result = "- - - - - - - - - - - - - - - - - - - - - -"
+	for i := range togos {
+		result = append(result, togos[i].ToString())
+	}
+	return
+}
+
+func (togos TogoList) Add(newTogo *Togo) TogoList {
+	return append(togos, *newTogo)
+}
+
+func (togos TogoList) ProgressMade() (progress float64, completedInPercent float64, completed uint64, extra uint64, total uint64) {
+	totalInPercent := uint64(0)
+	for i := range togos {
+		progress += float64(togos[i].Progress) * float64(togos[i].Weight)
+		if togos[i].Progress == 100 {
+			completed++
+			completedInPercent += float64(togos[i].Progress) * float64(togos[i].Weight)
+		}
+		if !togos[i].Extra {
+			totalInPercent += uint64(100 * togos[i].Weight)
+			total++
+		} else {
+			extra++
+		}
+	}
+	if totalInPercent > 0 {
+		progress *= 100 / float64(totalInPercent) // CHECK IF IT CALCULAFES DECIMAL PART OR NOT
+		completedInPercent *= 100 / float64(totalInPercent)
+	}
+	return
+}
+
+func (togos TogoList) Update(chatID int64, terms []string) string {
+	var id uint64
+	if _, err := fmt.Sscan(terms[0], &id); err != nil {
+		panic(err)
+	}
+	targetIdx := -1
+	// TODO: use simple version of FOR
+	for i := range togos {
+		if togos[i].Id == id {
+			targetIdx = i
+			break
+		}
+	}
+	if targetIdx < 0 {
+		return "There is no togo with this Id!"
+	}
+	if len(terms) > 1 && !isCommand(terms[1]) {
+
+		togos[targetIdx].setFields(terms)
+		togos[targetIdx].Update(chatID)
+	}
+
+	return togos[targetIdx].ToString()
+}
+func (togos *TogoList) RemoveIndex(index int) {
+	list := *togos
+	if len(list)-1 > index {
+		list = append(list[:index], list[index+1])
+	} else {
+		list = append(list[:index])
+	}
+	*togos = list
+}
+
+func (togos TogoList) Remove(ownerID int64, togoID uint64) error {
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	if _, err := db.Exec("DELETE FROM togos WHERE id=$1 AND owner_id=$2", togoID, ownerID); err != nil {
+		return err
+	}
+	for i := range togos {
+		if togos[i].Id == togoID && togos[i].OwnerId == ownerID {
+			togos.RemoveIndex(i)
+			break
+		}
+	}
+	return nil
+}
+
+func (togos TogoList) Get(togoID uint64) (*Togo, error) {
+	// TODO: use simple version of FOR
+	for i := range togos {
+		if togos[i].Id == togoID {
+			return &togos[i], nil
+		}
+	}
+	return nil, errors.New("Can not find this togo!")
+}
+
+// ---------------------- Shared Functions --------------------------------
+func Load(ownerId int64, justToday bool) (togos TogoList, err error) {
+
+	togos = make(TogoList, 0)
+	err = nil
+	if db, e := sql.Open("postgres", os.Getenv("POSTGRES_URL")); e == nil {
+		defer db.Close()
+		// ***** BETTER ALGORITHM
+		// FIRST GET THE COUNT OF ROWS, then create a slice of that size and then load into that.
+		const SELECT_QUERY string = "SELECT id, owner_id, title, description, weight, extra, progress, date, duration FROM togos WHERE owner_id=$1 ORDER BY date"
+		/* if justToday {
+			today := Date{time.Now()}
+			next := Date{today.AddDate(0, 0, 1)}
+			fmt.Println(next.Short())
+			SELECT_QUERY = fmt.Sprintf("%s WHERE date >= DATETIME(%s)", SELECT_QUERY, today.Short())//, next.Short())
+			fmt.Println(SELECT_QUERY)
+		}*/
+		rows, e := db.Query(SELECT_QUERY, ownerId)
+		if e != nil {
+			err = e
+			return
+		}
+
+		now := Today()
+		for rows.Next() {
+			var togo Togo
+			var date time.Time
+
+			err = rows.Scan(&togo.Id, &togo.OwnerId, &togo.Title, &togo.Description, &togo.Weight, &togo.Extra, &togo.Progress, &date, &togo.Duration)
+			if lastUsedId < togo.Id {
+				lastUsedId = togo.Id
+			}
+			if timeZone, err := time.LoadLocation("Asia/Tehran"); err == nil {
+				togo.Date = Date{date.In(timeZone)}
+			} else {
+				togo.Date = Date{date}
+			}
+			togo.Duration *= time.Minute
+			if err != nil {
+				panic(err)
+			}
+			if togo.Date.Short() == now.Short() {
+				if togo.Date.After(now.Time) {
+					togo.Schedule()
+				}
+				togos = togos.Add(&togo)
+			} else if !justToday {
+				togos = togos.Add(&togo)
+			}
+		}
+
+	} else {
+		err = e
+	}
+	return
+}
+
+func Extract(ownerId int64, terms []string) (togo Togo) {
+	// setting default values
+	if togo.Title = terms[0]; togo.Title == "" {
+		togo.Title = "Untitled"
+	}
+	togo.OwnerId = ownerId
+	togo.Weight = 1
+	togo.Date = Today()
+	(&togo).setFields(terms)
+	return
+}
---
+++ main.go
@@ -0,0 +1,399 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	Togo "github.com/pya-h/togo4bot/Togo"
+    chrono "github.com/gochrono/chrono"
+	"context"
+    "time"
+    "bufio"
+)
+
+const (
+	MaximumInlineButtonTextLength = 24
+	MaximumNumberOfRowItems       = 3
+	NumberOfSeparatorSpaces       = 2
+)
+
+// ---------------------- Telegram Response Struct & Interfaces --------------------------------
+type TelegramBotAPI interface {
+	tgbotapi.BotAPI
+	SendTextMessage(res *http.ResponseWriter)
+}
+
+type TelegramResponse struct {
+	TextMsg            string      `json:"text,omitempty"`
+	TargetChatID       int64       `json:"chat_id"`
+	Method             string      `json:"method"`
+	ReplyMarkup        ReplyMarkup `json:"reply_markup,omitempty"`
+	MessageRepliedTo   int         `json:"reply_to_message_id,omitempty"`
+	MessageBeingEdited int         `json:"message_id,omitempty"` // for edit message & etc
+	// file/photo?
+}
+
+type ReplyMarkup struct {
+	ResizeKeyboard bool                       `json:"resize_keyboard,omitempty"`
+	OneTime        bool                       `json:"one_time_keyboard,omitempty"`
+	Keyboard       [][]string                 `json:"keyboard,omitempty"`
+	InlineKeyboard [][]InlineKeyboardMenuItem `json:"inline_keyboard,omitempty"`
+}
+
+type InlineKeyboardMenuItem struct {
+	Text         string `json:"text"`
+	CallbackData string `json:"callback_data,omitempty"`
+	URL          string `json:"url,omitempty"`
+}
+
+func (self *TelegramBotAPI) SendTextMessage(response TelegramResponse) {
+    
+	msg := tgbotapi.NewMessage(response.TargetChatID, response.TextMsg)
+	msg.ReplyToMessageID =response.MessageRepliedTo
+
+	log.Printf("Response %s", string(msg))
+	self.send(msg)
+}
+
+func NewTelegramBotAPI(token string) (*TelegramBotAPI, error) {
+	bot, err := tgbotapi.NewBotAPI(token)
+	return &TelegramBotAPI{bot}, err
+}
+// ---------------------- Callback Structs & Functions --------------------------------
+type UserAction uint8
+
+const (
+	None UserAction = iota
+	TickTogo
+	UpdateTogo
+	RemoveTogo
+)
+
+type CallbackData struct {
+	Action  UserAction  `json:"A"`
+	ID      int64       `json:"ID,omitempty"`
+	Data    interface{} `json:"D,omitempty"`
+	AllDays bool        `json:"AD,omitempty"`
+}
+
+func (callbackData CallbackData) Json() string {
+	if res, err := json.Marshal(callbackData); err == nil {
+		return string(res)
+	} else {
+		return fmt.Sprint(err)
+	}
+}
+
+func LoadCallbackData(jsonString string) (data CallbackData) {
+	json.Unmarshal([]byte(jsonString), &data)
+	return
+}
+
+// ---------------------- Telegram Response Related Functions ------------------------------
+func InlineKeyboardMenu(togos Togo.TogoList, action UserAction, allDays bool) (menu ReplyMarkup) {
+	var (
+		count     = len(togos)
+		col       = 0
+		row       = 0
+		rowsCount = int(count / MaximumNumberOfRowItems)
+	) // calculate the number of rows needed
+	if count%MaximumNumberOfRowItems != 0 {
+		rowsCount++
+	}
+
+	menu.InlineKeyboard = make([][]InlineKeyboardMenuItem, rowsCount)
+
+	for i := range togos {
+		if col == 0 {
+			// calculting the number of column needed in each row
+			if row < rowsCount-1 {
+				menu.InlineKeyboard[row] = make([]InlineKeyboardMenuItem, MaximumNumberOfRowItems)
+			} else {
+				menu.InlineKeyboard[row] = make([]InlineKeyboardMenuItem, count-row*MaximumNumberOfRowItems)
+			}
+			row++
+		}
+		status := ""
+		if togos[i].Progress >= 100 {
+			status = "✅ "
+		}
+		var togoTitle string = fmt.Sprint(status, togos[i].Title)
+		if len(togoTitle) >= MaximumInlineButtonTextLength {
+			togoTitle = fmt.Sprint(togoTitle[:MaximumInlineButtonTextLength], "...")
+		}
+		menu.InlineKeyboard[row-1][col] = InlineKeyboardMenuItem{Text: togoTitle,
+			CallbackData: (CallbackData{Action: action, ID: int64(togos[i].Id), AllDays: allDays}).Json()}
+		col = (col + 1) % MaximumNumberOfRowItems
+	}
+	return
+}
+
+func MainKeyboardMenu() ReplyMarkup {
+	return ReplyMarkup{ResizeKeyboard: true,
+		OneTime:  false,
+		Keyboard: [][]string{{"#", "%"}, {"#  -a", "%  -a"}, {"✅"}, {"❌", "❌  -a"}}}
+}
+
+// ---------------------- tgbotapi Related Functions ------------------------------
+func GetTgBotApiFunction(update *tgbotapi.Update) func(data string) error {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	return func(data string) error {
+		if err == nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, data)
+			// msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+			return nil
+		}
+		return err
+	}
+}
+
+// ---------------------- Togos Related Functions ------------------------------
+func LoadForToday(chatId int64, togos *Togo.TogoList) {
+	tg, err := Togo.Load(chatId, true) // load today's togos,  make(Togo.TogoList, 0)
+	if err != nil {
+		fmt.Println("Loading failed: ", err)
+	}
+	*togos = tg
+
+	/*
+		today := time.Now()
+		// mainTaskScheduler.Schedule(func(ctx context.Context) { LoadForToday(togos) },
+		// 	chrono.WithStartTime(today.Year(), today.Month(), today.Day()+1, 0, 0, 0))
+	*/
+}
+func SplitArguments(statement string) []string {
+	result := make([]string, 0)
+	numOfSpaces := 0
+	segmentStartIndex := 0
+
+	for i := range statement {
+		if statement[i] == ' ' {
+			numOfSpaces++
+		} else if numOfSpaces > 0 {
+			if numOfSpaces == NumberOfSeparatorSpaces {
+				result = append(result, statement[segmentStartIndex:i-NumberOfSeparatorSpaces])
+				segmentStartIndex = i
+			}
+			numOfSpaces = 0
+		}
+
+	}
+	result = append(result, statement[segmentStartIndex:])
+	return result
+}
+
+func Log(update *tgbotapi.Update, values []string) {
+	sendMessage := GetTgBotApiFunction(update)
+	var r string
+	for _, v := range values {
+		r += v + "\n"
+	}
+	sendMessage(r)
+}
+
+var mainTaskScheduler chrono.TaskScheduler = chrono.NewDefaultTaskScheduler()
+
+func autoLoad(togos *Togo.TogoList) {
+	tg, err := Togo.Load(true) // load today's togos,  make(Togo.TogoList, 0)
+	if err != nil {
+		fmt.Println("Loading failed: ", err)
+	}
+	*togos = tg
+	today := time.Now()
+	mainTaskScheduler.Schedule(func(ctx context.Context) { autoLoad(togos) },
+		chrono.WithStartTime(today.Year(), today.Month(), today.Day()+1, 0, 0, 0))
+
+}
+
+// ---------------------- Serverless Function ------------------------------
+func main() {
+	var togos Togo.TogoList
+
+	var update tgbotapi.Update
+
+	bot, err := NewTelegramBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	//if update.Message.IsCommand() {
+	response := TelegramResponse{TextMsg: "What?",
+		Method: "sendMessage"} // default method is sendMessage
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			response.TextMsg = fmt.Sprintln("❌: ", err)
+			response.CallAPI(&res)
+		}
+	}()
+
+	// ---------------------- Handling Casual Telegram text Messages ------------------------------
+	if update.Message != nil { // If we got a message
+		response.ReplyMarkup = MainKeyboardMenu() // default keyboard
+		response.TargetChatID = update.Message.Chat.ID
+		response.MessageRepliedTo = update.Message.MessageID
+		// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		LoadForToday(update.Message.Chat.ID, &togos)
+
+		terms := SplitArguments(update.Message.Text)
+		// Log(&update, terms)
+		numOfTerms := len(terms)
+		var now Togo.Date = Togo.Today()
+		for i := 0; i < numOfTerms; i++ {
+			switch terms[i] {
+			case "+":
+				if numOfTerms > 1 {
+
+					togo := Togo.Extract(update.Message.Chat.ID, terms[i+1:])
+					togo.Id = togo.Save()
+					if togo.Date.Short() == now.Short() {
+						togos = togos.Add(&togo)
+						if togo.Date.After(now.Time) {
+							togo.Schedule()
+						}
+					}
+					response.TextMsg = fmt.Sprint(now.Get(), ": DONE!")
+				} else {
+					response.TextMsg = "You must provide at least one Parameters!"
+				}
+			case "#":
+				var results []string
+				if i+1 < numOfTerms && terms[i+1] == "-a" {
+					allTogos, err := Togo.Load(update.Message.Chat.ID, false)
+					if err != nil {
+						panic(err)
+					}
+					results = allTogos.ToString()
+				} else {
+					results = togos.ToString()
+				}
+				var waiter sync.WaitGroup
+				sendMessage := GetTgBotApiFunction(&update)
+				if len(results) > 0 {
+					for i := range results {
+						waiter.Add(1)
+						// newBug: result its not sorted by time
+						// possible fix: collect all togos in a day as single message
+						go func(data string) {
+							defer waiter.Done()
+							sendMessage(data)
+						} (results[i])
+					}
+					response.TextMsg = "✅!"
+				} else {
+					response.TextMsg = "Nothing!"
+				}
+				waiter.Wait()
+			case "%":
+				var target *Togo.TogoList = &togos
+				scope := "Today's"
+				if i+1 < numOfTerms && terms[i+1] == "-a" {
+					allTogos, err := Togo.Load(update.Message.Chat.ID, false)
+					if err != nil {
+						panic(err)
+					}
+					target = &allTogos
+					scope = "Total"
+				}
+				progress, completedInPercent, completed, extra, total := (*target).ProgressMade()
+				response.TextMsg = fmt.Sprintf("%s Progress: %3.2f%% \n%3.2f%% Completed\nStatistics: %d / %d",
+					scope, progress, completedInPercent, completed, total)
+				if extra > 0 {
+					response.TextMsg = fmt.Sprintf("%s[+%d]\n", response.TextMsg, extra)
+				}
+			case "$":
+				// set or update a togo
+				if i+1 < numOfTerms {
+					if terms[i+1] == "-a" {
+						if i+2 < numOfTerms {
+							allTogos, err := Togo.Load(update.Message.Chat.ID, false)
+							if err != nil {
+								panic(err)
+							}
+							response.TextMsg = allTogos.Update(update.Message.Chat.ID, terms[i+2:])
+						} else {
+							response.TextMsg = "You must provide at least one Parameters!"
+						}
+					} else {
+						response.TextMsg = togos.Update(update.Message.Chat.ID, terms[i+1:])
+
+					}
+				} else {
+					response.TextMsg = "You must provide at least one Parameters!"
+				}
+			// TODO: write Tick command
+			case "✅":
+				response.TextMsg = "Here are your togos for today:"
+				response.ReplyMarkup = InlineKeyboardMenu(togos, TickTogo, false)
+			case "❌":
+				if i+1 < numOfTerms && terms[i+1] == "-a" {
+					allTogos, err := Togo.Load(update.Message.Chat.ID, false)
+					if err != nil {
+						panic(err)
+					}
+					response.TextMsg = "Here are your ALL togos:"
+					response.ReplyMarkup = InlineKeyboardMenu(allTogos, RemoveTogo, true)
+				} else {
+					response.TextMsg = "Here are your togos for today:"
+					response.ReplyMarkup = InlineKeyboardMenu(togos, RemoveTogo, false)
+				}
+			case "/now":
+				response.TextMsg = now.Get()
+
+			}
+
+		}
+		response.CallAPI(&res)
+
+	} else if update.CallbackQuery != nil {
+		response.MessageBeingEdited = update.CallbackQuery.Message.MessageID
+		response.TargetChatID = update.CallbackQuery.Message.Chat.ID
+		response.Method = "editMessageText"
+
+		callbackData := LoadCallbackData(update.CallbackQuery.Data)
+		if !callbackData.AllDays {
+			LoadForToday(response.TargetChatID, &togos)
+		} else {
+			var err error
+			togos, err = Togo.Load(response.TargetChatID, false)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		switch callbackData.Action {
+		case TickTogo:
+			togo, err := togos.Get(uint64(callbackData.ID))
+			if err != nil {
+				panic(err)
+			} else {
+				if (*togo).Progress < 100 {
+					(*togo).Progress = 100
+				} else {
+					(*togo).Progress = 0
+				}
+				(*togo).Update(response.TargetChatID)
+				response.ReplyMarkup = InlineKeyboardMenu(togos, TickTogo, false)
+				response.TextMsg = "✅ DONE! Now select the next togo you want to tick ..."
+			}
+		case RemoveTogo:
+			err := togos.Remove(response.TargetChatID, uint64(callbackData.ID))
+			if err == nil {
+				response.TextMsg = "❌ DONE! Now select the next togo you want to REMOVE ..."
+				response.ReplyMarkup = InlineKeyboardMenu(togos, RemoveTogo, callbackData.AllDays)
+
+			} else {
+				panic(err)
+			}
+		}
+		bot.Send
+	}
+
+}
